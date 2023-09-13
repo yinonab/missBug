37b92509643e5daf4413fb5ae68aca26aa020cf3
yinonab <yinonabarjel@gmail.com>
2023-09-13 23:20:30 +0300

    pass bugs to user tetailed

--- public/pages/UserDetailes.jsx
+++ public/pages/UserDetailes.jsx
@@ -1,41 +1,50 @@
+import { userService } from '../services/user.service.js';
+import { showErrorMsg } from '../services/event-bus.service.js';
+import { BugIndex } from './BugIndex.jsx';
+import { bugService } from '../services/bug.service.js';
 
-import { userService } from '../services/user.service.js'
-import { showErrorMsg } from '../services/event-bus.service.js'
-import { BugIndex } from './BugIndex.jsx'
-import { bugService } from '../services/bug.service.js'
-
-const { useState, useEffect } = React
-const { Link, useParams } = ReactRouterDOM
+const { useState, useEffect } = React;
+const { Link, useParams } = ReactRouterDOM;
 
 export function UserDetails() {
-
-    const [user, setUser] = useState(null)
-    console.log('user', user);
-    const [bugs, setBugs] = useState(null)
-    // console.log('bugs', bugs);
-    // const { userId } = useParams()
-    // console.log('userId', userId);
+    const [user, setUser] = useState(null);
+    const [bugs, setBugs] = useState([]);
+    const { userId } = useParams();
 
     useEffect(() => {
-       const logginUser =  userService.getLoggedinUser()
-        setUser(logginUser)
-        setBugs(bugService.getUserBugs(logginUser))
-        // const bugs =bugService.getUserBugs(userId)
-        // bugs.then(bugs => setBugs(bugs))
-        // // .then(bugs => setBugs(bugs))
-        // console.log('bugs:', bugs)
-    }, [])
-    // useEffect(() => {
-    //     bugService.getUserBugs(userId)
-    //     .then(bugs => setBugs(bugs))
-    // }, [])
+        // Fetch user information
+        const loggedInUser = userService.getLoggedinUser();
+        setUser(loggedInUser);
 
-    if (!user) return <h1>loadings....</h1>
-    return user && <div>
-        <h3>User Details</h3>
-        <h4> Full Name :{user.fullname}</h4>
-        <Link to="/bug">Back to List</Link>
-    </div>
+        if (loggedInUser) {
+            // Fetch the user's bugs using their ID
+            bugService.getUserBugs(loggedInUser._id)
+                .then(bugs => setBugs(bugs))
+                .catch(error => {
+                    console.error('Error fetching user bugs:', error);
+                });
+        }
+    }, []);
 
+    if (!user) return <h1>Loading...</h1>;
+
+    return (
+        <div>
+            <h3>User Details</h3>
+            <h4>Full Name: {user.fullname}</h4>
+            
+            <h3>User's Bugs</h3>
+            {bugs.length > 0 ? (
+                <ul>
+                    {bugs.map(bug => (
+                        <li key={bug._id}>{bug.title}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No bugs found for this user.</p>
+            )}
+            
+            <Link to="/bug">Back to List</Link>
+       </div>
+);
 }
-
